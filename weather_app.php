8878466cfdbc6dbd1e96772a04c70686bedb11fd
isab95 <noelisabelle15@gmail.com>
2024-06-21 10:11:00 +0200

    possible to add new city to database

--- weather_app.php
+++ weather_app.php
@@ -5,6 +5,23 @@ try {
     $stmt = $pdo->prepare('SELECT * FROM météo');
     $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //Add data in database
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if (isset($_POST['city']) && isset($_POST['high']) && isset($_POST['low'])) {
+            $querry = $pdo->prepare("INSERT INTO météo (ville, haut, bas) VALUES (:city, :high, :low)");
+            $querry->bindParam(':city', $_POST['city'], PDO::PARAM_STR);
+            $querry->bindParam(':high', $_POST['high'], PDO::PARAM_INT);
+            $querry->bindParam(':low', $_POST['low'], PDO::PARAM_INT);
+            if(!$querry->execute()){
+                echo "something don't work";
+            }
+            else{
+                header('Location: ' . $_SERVER['PHP_SELF']);
+            }
+        }
+    }
+
 }
 catch(PDOException $e) {
     $msg = 'ERREUR PDO dans ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
@@ -12,7 +29,8 @@ catch(PDOException $e) {
 }
 ?>
 
-        <h1>Weather app</h1>
+    <h1>Weather app</h1>
+    <form action="" method="POST">
         <table>
             <thead>
                 <tr>
@@ -32,6 +50,12 @@ catch(PDOException $e) {
                     </tr>
 
                 <?php endforeach; ?>
-
+                <tr>
+                    <td><input type="text" name="city" required></td>
+                    <td><input type="number" name="high" required></td>
+                    <td><input type="number" name="low" required></td>
+                </tr>
+                    <button type='submit'>Add</button>
             </tbody>
         </table>
+    </form>
